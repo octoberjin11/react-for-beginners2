e7805a4ac7774e2b52e448612d5b907f5574873a
진희정 <citron@mz.co.kr>
2022-08-21 22:06:28 +0900

    To Do List part Two

--- src/App.js
+++ src/App.js
@@ -1,26 +1,5 @@
 import { useState } from "react";
 
-/*
-  form은 submit 이벤트를 갖고 있다.
-  그러므로 evernt.preventDefault() 함수를 이용하여 기본 동작을 막자.
-  여러 개의 toDo를 받을 수 있는 배열 만들기
-  const [toDos, setToDos] = useState([]); -> 기본값은 비어있는 배열
-  state는 직접적으로 수정 불가능 (예 : toDo = “” )
-  함수를 가져와서 수정하게 만들어야함 (예 : setToDo = (“”) )
-  그래서 toDos 배열을 수정하고 싶다면 수정하는 함수를 써야함
-
-  setToDos(currentArray => [toDo, ...currentArray]);
-  -> ...을 써서 currentArray 배열에 toDo를 추가 시켜줌
-  어플리케이션이 시작될 때는 비어있는 배열을 가짐
-  첫 번째 to-do를 입력할 때 비어있는 currentArray 받아옴
-  이건 새로운 toDos가 input을 통해 작성한 toDo와
-  아무것도 들어있지 않은 빈 배열의 element가 더해지게 된다는 것
-  첫 번째 toDo 가 Hello라면 엔터를 눌러 실행됨
-  그리고 byebye라고 적으면
-  currentArray에는 Hello 이미 있고 toDo는 byebye가 되는 것
-  그리고 currentArray는 Hello와 byebye를 가지고 있는 배열이 됨
-*/
-
 function App() {
   const [toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
@@ -35,6 +14,12 @@ function App() {
   };
   console.log(toDos);
 
+  // map은 하나의 array에 있는 item을 내가 원하는 무엇이든지로 바꿔주는 역할. 그건 결국 새로운 array로 반환해준다.
+
+  // 같은 component의 list를 render 할 때 key라는 prop을 넣어줘야 한다. 이건 그냥 react가 기본적으로 list에 있는 모든 item들을 인식하기 때문이다.
+  // 리액트는 기본적으로 list에 있는 모든 item을 인식하기 때문에 key를 넣어 고유하게 만들어줘야함
+  // map의 첫 번째 argument는 값이고 두번째는 index 즉 숫자를 의미함
+
   return (
     <div>
       <h1>Mt To Dos ({toDos.length})</h1>
@@ -47,6 +32,12 @@ function App() {
         />
         <button>Add to Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
