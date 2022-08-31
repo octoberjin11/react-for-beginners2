c8a88a2643304b19a10a33a19a678ec76b90d5df
진희정 <citron@mz.co.kr>
2022-08-31 22:44:22 +0900

    Movie App part One - async await 1

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    fetch(
+  const getMovies = async () => {
+    const response = await fetch(
       "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
   }, []);
 
   console.log(movies);
