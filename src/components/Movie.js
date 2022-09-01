d25cff4875abcda71b8f57ee28318bc2be590442
진희정 <citron@mz.co.kr>
2022-09-02 01:24:05 +0900

    Parameters

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,13 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, summary, genres }) {
   //Movie component가 { coverImg, title, summary, genres } 이 정보들을 parent component로부터 받아온다는 뜻.
   return (
     <div>
       <img src={coverImg} alt={title} />
       <h2>
-        <Link to="/movie">{title}</Link>
+        <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
       <p>{summary}</p>
       <ul>
@@ -21,6 +21,7 @@ function Movie({ coverImg, title, summary, genres }) {
 }
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+// useParams 함수를 사용하면 React Router는 바로 이 변수(여기에서는 <Link to={`/movie/${id}`}>{title}</Link>의 id)의 값을 넘겨준다.
+
 function Detail() {
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
   return <h1>Detail</h1>;
 }
 
